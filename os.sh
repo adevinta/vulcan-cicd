9f6b0c31f7046728938ef0a47d9af5e153f0ba21
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-03-12 11:33:36 +0100

    compatible mac

--- os.sh
+++ os.sh
@@ -14,9 +14,13 @@ function patch_folder() {
         exit 1
     fi
 
+    SED=sed
     if [[ "$(uname)" == "Darwin" ]]; then
-        echo "Not compatible with Macos"
-        exit 1
+        if [[ ! -x "$(which gsed)" ]]; then
+            echo "We need to use gnu-sed in mac (brew install gsed)"
+            exit 1
+        fi
+        SED=gsed
     fi
 
     COMMON_MSG="Copyright $(date +"%Y") Adevinta"
@@ -24,21 +28,21 @@ function patch_folder() {
 
     # Update existing year
     rg -l -e ''$COMMON_PATTERN'' $BASE | \
-        xargs -r -n1 sed -i 's|Copyright[ \t]\+20[0-9]\{2\}[ \t]\+Adevinta|'"$COMMON_MSG"'|g'
+        xargs -r -n1 $SED -i 's|Copyright[ \t]\+20[0-9]\{2\}[ \t]\+Adevinta|'"$COMMON_MSG"'|g'
 
     # Files with hash-bang notation (shell, python, ruby)
     COMMENT="# $COMMON_MSG\n"
     rg --files-without-match ''"$COMMON_PATTERN"'' -t py -t sh -t ruby $BASE | \
-        xargs -r -n1 sed -i '1s|^\(#!/.\+\)|\1\n'"$COMMENT"'|'
+        xargs -r -n1 $SED -i '1s|^\(#!/.\+\)|\1\n'"$COMMENT"'|'
 
     COMMENT="# $COMMON_MSG\n\n"
     rg --files-without-match ''"$COMMON_PATTERN"'' \
         -t ruby -t py -t docker -t sh -t yaml -t toml -g '!.travis.y*ml' $BASE | \
-        xargs -r -n1 sed -i '1s|^|'"$COMMENT"'|'
+        xargs -r -n1 $SED -i '1s|^|'"$COMMENT"'|'
 
     COMMENT="/*\n$COMMON_MSG\n*/\n"
     rg --files-without-match ''"$COMMON_PATTERN"'' -t go $BASE | \
-        xargs -r -n1 sed -i '1s|^|'"$COMMENT"'|'
+        xargs -r -n1 $SED -i '1s|^|'"$COMMENT"'|'
 }
 
 function add_copyright() {
