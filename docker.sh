26fec46bb4ff315b16fc3dfea49b2920eeeeb118
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-06-04 18:12:47 +0200

    Refactor

--- docker.sh
+++ docker.sh
@@ -7,74 +7,64 @@ function docker_init() {
   echo "" # see https://github.com/actions/toolkit/issues/168
 
   # Aapt to be compatible with artifactory registry
-  INPUT_REGISTRY=${INPUT_REGISTRY:-$ARTIFACTORY_DOCKER_REGISTRY}
-  INPUT_USERNAME=${INPUT_USERNAME:-$ARTIFACTORY_USER}
-  INPUT_PASSWORD=${INPUT_PASSWORD:-$ARTIFACTORY_PWD}
+  _DKR_REGISTRY=$ARTIFACTORY_DOCKER_REGISTRY
+  _DKR_USERNAME=${DOCKER_USERNAME:-$ARTIFACTORY_USER}
+  _DKR_PASSWORD=${DOCKER_PASSWORD:-$ARTIFACTORY_PWD}
+  _DKR_REPO=${GITHUB_REPOSITORY:-$TRAVIS_REPO_SLUG}
 
-  # Adapt to be compatible with travis-ci
-  INPUT_USERNAME=${INPUT_USERNAME:-$DOCKER_USERNAME}
-  INPUT_PASSWORD=${INPUT_PASSWORD:-$DOCKER_PASSWORD}
-  INPUT_NAME=${INPUT_NAME:-$GITHUB_REPOSITORY}
-  INPUT_NAME=${INPUT_NAME:-$TRAVIS_REPO_SLUG}
-  GITHUB_SHA=${GITHUB_SHA:-$TRAVIS_COMMIT}
-  GITHUB_REF=${GITHUB_REF:-$TRAVIS_PULL_REQUEST_BRANCH}   # In case of pull request we want the originating branch, not the target.
-  GITHUB_REF=${GITHUB_REF:-$TRAVIS_BRANCH}
-
-  GITHUB_SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-7)
-
-  echo "INPUT_NAME=${INPUT_NAME}"
-  echo "GITHUB_SHA=${GITHUB_SHA}"
-  echo "GITHUB_REF=${GITHUB_REF}"
-
-  sanitize "${INPUT_NAME}" "name"
-  sanitize "${INPUT_USERNAME}" "username"
-  sanitize "${INPUT_PASSWORD}" "password"
-
-  REGISTRY_NO_PROTOCOL=$(echo "${INPUT_REGISTRY}" | sed -e 's/^https:\/\///g')
-  if [ -n "$INPUT_REGISTRY" ] && [[ ${INPUT_NAME} == *${REGISTRY_NO_PROTOCOL}* ]]; then
-    INPUT_NAME="${REGISTRY_NO_PROTOCOL}/${INPUT_NAME}"
+  _GIT_SHA=${GITHUB_SHA:-$TRAVIS_COMMIT}
+  if [[ "$GITHUB_REF" =~ 'refs/_DKR_TAGS/' ]]; then
+    _GIT_TAG=${GITHUB_REF//refs\/_DKR_TAGS\//}
+  else
+    _GIT_TAG=${_GIT_TAG:-TRAVIS_TAG}
   fi
 
-  echo "${INPUT_PASSWORD}" | docker login -u "${INPUT_USERNAME}" --password-stdin "${INPUT_REGISTRY}"
+  # If it's a pr the head/originator branch.
+  _GIT_BRANCH=${GITHUB_HEAD_REF:-TRAVIS_PULL_REQUEST_BRANCH}
 
-  if [ -n "$INPUT_TAGS" ]; then
-    TAGS=${INPUT_TAGS//,/ }
-  else 
-    local BRANCH
-    BRANCH=$(echo "${GITHUB_REF}" | sed -e "s|refs/heads|/|g" | sed -e "s|/|-|g")
+  if [[ "$GITHUB_REF" =~ 'refs/heads/' ]]; then
+    _GIT_BRANCH=${GITHUB_REF//refs\/heads\//}
+  else
+    _GIT_BRANCH=$TRAVIS_BRANCH
+  fi
 
-    # Has a custom tag
-    if [[ $INPUT_NAME =~ ':' ]]; then
-      TAGS=$(echo "${INPUT_NAME}" | cut -d':' -f2)
-      INPUT_NAME=$(echo "${INPUT_NAME}" | cut -d':' -f1)
-    elif [ "${BRANCH}" = "master" ]; then
-      TAGS="latest latest-${GITHUB_SHORT_SHA}"
-    elif [[ "$GITHUB_REF" =~ 'refs/tags' ]] && [ "${INPUT_TAG_NAMES}" = "true" ]; then
-      TAGS=${GITHUB_REF//refs\/tags/}
-    elif [[ "$GITHUB_REF" =~ 'refs/tags' ]]; then
-      TAGS="latest"
-    elif [[ "$GITHUB_REF" =~ 'refs/pull' ]]; then
-      TAGS="${GITHUB_SHA}"
-    else
-      TAGS="${BRANCH} ${BRANCH}-${GITHUB_SHORT_SHA}"
-    fi;
+  _GIT_SHORT_SHA=$(echo "${_GIT_SHA}" | cut -c1-7)
 
-    # Always tag with the full SHA
-    if [[ ! $TAGS =~ $GITHUB_SHA ]]; then
-      TAGS="${TAGS} ${GITHUB_SHA}"
+  echo "_DKR_REPO=${_DKR_REPO}"
+  echo "_GIT_SHA=${_GIT_SHA}"
+  echo "_GIT_BRANCH=${_GIT_BRANCH}"
+
+  sanitize "${_DKR_REPO}" "name"
+  sanitize "${_DKR_USERNAME}" "username"
+  sanitize "${_DKR_PASSWORD}" "password"
+
+  local REGISTRY_NO_PROTOCOL
+  REGISTRY_NO_PROTOCOL=$(echo "${_DKR_REGISTRY}" | sed -e 's/^https:\/\///g')
+  if [ -n "$_DKR_REGISTRY" ] && [[ ${_DKR_REPO} == *${REGISTRY_NO_PROTOCOL}* ]]; then
+    _DKR_REPO="${REGISTRY_NO_PROTOCOL}/${_DKR_REPO}"
+  fi
+
+  echo "${_DKR_PASSWORD}" | docker login -u "${_DKR_USERNAME}" --password-stdin "${_DKR_REGISTRY}"
+
+  local BRANCH
+
+  if [ -n "$_GIT_TAG" ]; then
+    _DKR_TAGS=$_GIT_TAG
+  elif [ -n "$_GIT_BRANCH" ]; then
+    BRANCH=${_GIT_BRANCH//\//-}
+    if [ "$BRANCH" = "master" ]; then
+      BRANCH=latest
     fi
-
-  fi
+    _DKR_TAGS="${BRANCH} ${BRANCH}-${_GIT_SHORT_SHA} ${_GIT_SHA}"
+  else
+    _DKR_TAGS="${_GIT_SHA}"
+  fi;
 
   if [ "${INPUT_SNAPSHOT}" = "true" ]; then
-    local TIMESTAMP
-    TIMESTAMP=$(date +%Y%m%d%H%M%S)
-    local SNAPSHOT_TAG="${TIMESTAMP}${GITHUB_SHORT_SHA}"
-    TAGS="${TAGS} ${SNAPSHOT_TAG}"
-    echo ::set-output name=snapshot-tag::"${SNAPSHOT_TAG}"
+    _DKR_TAGS="${_DKR_TAGS} $(date +%Y%m%d%H%M%S)${_GIT_SHORT_SHA}"
   fi
 
-  echo "TAGS=$TAGS"
+  echo "_DKR_TAGS=$_DKR_TAGS"
 }
 
 function sanitize() {
@@ -89,14 +79,9 @@ function docker_build() {
     cd "${INPUT_WORKDIR}"
   fi
 
-  FIRST_TAG=$(echo "$TAGS" | cut -d ' ' -f1)
-  DOCKERNAME="${INPUT_NAME}:${FIRST_TAG}"
-  BUILDPARAMS="--build-arg BUILD_RFC3339=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --build-arg COMMIT=$GITHUB_SHA"
+  FIRST_TAG=$(echo "$_DKR_TAGS" | cut -d ' ' -f1)
+  BUILDPARAMS="--build-arg BUILD_RFC3339=$(date -u +"%Y-%m-%dT%H:%M:%SZ") --build-arg COMMIT=$_GIT_SHA"
   CONTEXT="."
-
-  if [ "${DOCKER_BUILDKIT}" = "1" ]; then
-    BUILDPARAMS="${BUILDPARAMS} --progress=plain"
-  fi
 
   if [ -n "${INPUT_BUILDOPTIONS}" ]; then
     BUILDPARAMS="${BUILDPARAMS} ${INPUT_BUILDOPTIONS}"
@@ -108,36 +93,26 @@ function docker_build() {
   if [ -n "${INPUT_BUILDARGS}" ]; then
     for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
       BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}"
-      echo "::add-mask::${ARG}"
     done
   fi
   if [ -n "${INPUT_CONTEXT}" ]; then
     CONTEXT="${INPUT_CONTEXT}"
   fi
-  if [ "${INPUT_CACHE}" = "true" ]; then
-    if docker pull "${DOCKERNAME}" 2>/dev/null; then
-      BUILDPARAMS="$BUILDPARAMS --cache-from ${DOCKERNAME}"
-    fi
-  fi
 
-  local BUILD_TAGS=""
-  for TAG in ${TAGS}
+  local BUILDTAGS=""
+  for TAG in ${_DKR_TAGS}
   do
-    BUILD_TAGS="${BUILD_TAGS}-t ${INPUT_NAME}:${TAG} "
+    BUILDTAGS="${BUILDTAGS}-t ${_DKR_REPO}:${TAG} "
   done
-  echo "docker build ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}"
-  eval "docker build ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}"
+  echo "docker build ${BUILDPARAMS} ${BUILDTAGS} ${CONTEXT}"
+  eval "docker build ${BUILDPARAMS} ${BUILDTAGS} ${CONTEXT}"
 }
 
 function docker_push() {
-  for TAG in ${TAGS}
+  for TAG in ${_DKR_TAGS}
   do
-    eval "docker push ${INPUT_NAME}:${TAG}"
+    eval "docker push ${_DKR_REPO}:${TAG}"
   done
-
-  echo "::set-output name=tag::${FIRST_TAG}"
-  DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' ${DOCKERNAME})
-  echo "::set-output name=digest::${DIGEST}"
 }
 
 function docker_all() {
